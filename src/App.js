185f753152bec1044bc89211afb2d267d14e084a
rachel490 <ccori12@naver.com>
2022-11-13 23:57:40 +0900

    feat: 로또 구입 금액 예외처리 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
+
 class App {
-  play() {}
+	play() {
+		Console.readLine('구입금액을 입력해 주세요.\n', received_money => {
+			this.validateAmount(+received_money);
+		});
+	}
+	validateAmount(money) {
+		if (typeof money !== 'number' || isNaN(money)) {
+			throw Error('[ERROR] 유효한 숫자를 입력해주세요.');
+		}
+		if (money < 1000) {
+			throw Error('[ERROR] 1000원 이상의 금액을 입력해주세요');
+		}
+		if (money % 1000 !== 0) {
+			throw Error('[ERROR] 1000원 단위로 금액을 입력해주세요.');
+		}
+	}
 }
 
 module.exports = App;
