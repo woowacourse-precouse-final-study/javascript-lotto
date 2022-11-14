0519774cc21e9daac02e7c5d62b42dab5b717ded
rachel490 <ccori12@naver.com>
2022-11-15 08:22:52 +0900

    refactor: App 메소드 순서 변경

--- src/App.js
+++ src/App.js
@@ -12,10 +12,6 @@ class App {
 		this.getPayment();
 	}
 
-	close() {
-		Console.close();
-	}
-
 	getPayment() {
 		Console.readLine('구입금액을 입력해 주세요.\n', received_money => {
 			this.validateAmount(+received_money);
@@ -30,54 +26,6 @@ class App {
 		Console.print(`\n${quantity}개를 구매했습니다.`);
 
 		this.generateLottos(quantity);
-	}
-
-	validateAmount(money) {
-		if (typeof money !== 'number' || isNaN(money)) {
-			throw Error('[ERROR] 유효한 숫자를 입력해주세요.');
-		}
-		if (money < 1000) {
-			throw Error('[ERROR] 1000원 이상의 금액을 입력해주세요');
-		}
-		if (money % 1000 !== 0) {
-			throw Error('[ERROR] 1000원 단위로 금액을 입력해주세요.');
-		}
-	}
-
-	validateWinningNumber(numbers) {
-		if (numbers.split(',').length !== 6) {
-			throw Error('[ERROR] , 로 구분 6개의 숫자를 입력해주세요.');
-		}
-		const numberList = numbers.split(',').map(str => +str);
-
-		numberList.map((number, idx) => {
-			if (typeof number !== 'number') {
-				throw Error('[ERROR] 숫자를 입력해주세요.');
-			}
-			if (number < 1 && number > 45) {
-				throw Error('[ERROR] 1부터 45 사이의 번호를 입력해주세요.');
-			}
-			if (numberList.slice(idx + 1).includes(number)) {
-				throw Error('[ERROR] 중복된 숫자가 있습니다.');
-			}
-		});
-
-		return numberList;
-	}
-
-	validateBonusNumber(number) {
-		if (typeof +number !== 'number' || isNaN(+number)) {
-			throw Error('[ERROR] 숫자를 입력해주세요.');
-		}
-		if (number < 1 && number > 45) {
-			throw Error('[ERROR] 1부터 45 사이의 번호를 입력해주세요.');
-		}
-
-		if (this.#winning_number.includes(number)) {
-			throw Error('[ERROR] 당첨번호 6개에 없는 숫자를 입력해주세요.');
-		}
-
-		return +number;
 	}
 
 	generateLottos(cnt) {
@@ -163,6 +111,58 @@ class App {
 
 		Console.print(`총 수익률은 ${profit}%입니다.`);
 		this.close();
+	}
+
+	close() {
+		Console.close();
+	}
+
+	validateAmount(money) {
+		if (typeof money !== 'number' || isNaN(money)) {
+			throw Error('[ERROR] 유효한 숫자를 입력해주세요.');
+		}
+		if (money < 1000) {
+			throw Error('[ERROR] 1000원 이상의 금액을 입력해주세요');
+		}
+		if (money % 1000 !== 0) {
+			throw Error('[ERROR] 1000원 단위로 금액을 입력해주세요.');
+		}
+	}
+
+	validateWinningNumber(numbers) {
+		if (numbers.split(',').length !== 6) {
+			throw Error('[ERROR] , 로 구분 6개의 숫자를 입력해주세요.');
+		}
+		const numberList = numbers.split(',').map(str => +str);
+
+		numberList.map((number, idx) => {
+			if (typeof number !== 'number') {
+				throw Error('[ERROR] 숫자를 입력해주세요.');
+			}
+			if (number < 1 && number > 45) {
+				throw Error('[ERROR] 1부터 45 사이의 번호를 입력해주세요.');
+			}
+			if (numberList.slice(idx + 1).includes(number)) {
+				throw Error('[ERROR] 중복된 숫자가 있습니다.');
+			}
+		});
+
+		return numberList;
+	}
+
+	validateBonusNumber(number) {
+		if (typeof +number !== 'number' || isNaN(+number)) {
+			throw Error('[ERROR] 숫자를 입력해주세요.');
+		}
+		if (number < 1 && number > 45) {
+			throw Error('[ERROR] 1부터 45 사이의 번호를 입력해주세요.');
+		}
+
+		if (this.#winning_number.includes(number)) {
+			throw Error('[ERROR] 당첨번호 6개에 없는 숫자를 입력해주세요.');
+		}
+
+		return +number;
 	}
 }
 
