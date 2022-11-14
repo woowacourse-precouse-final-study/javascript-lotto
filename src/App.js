f8994b19f69ae9b5f1983e15e0854d886625dbdc
rachel490 <ccori12@naver.com>
2022-11-14 23:58:25 +0900

    feat: 로또 발행 및 출력

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ class App {
 	play() {
 		Console.readLine('구입금액을 입력해 주세요.\n', received_money => {
 			this.validateAmount(+received_money);
-      
+
 			const lotto_cnt = received_money / 1000;
 			Console.print(`\n${lotto_cnt}개를 구매했습니다.\n`);
+
+			const lottos = this.generateLottos(lotto_cnt);
+      lottos.map(loto => Console.print(loto));
 		});
 	}
 
@@ -21,6 +24,21 @@ class App {
 		if (money % 1000 !== 0) {
 			throw Error('[ERROR] 1000원 단위로 금액을 입력해주세요.');
 		}
+	}
+
+	generateLottos(cnt) {
+		const lottos = [];
+
+		Array(cnt)
+			.fill(0)
+			.map(() => {
+				const random_number = Random.pickUniqueNumbersInRange(1, 45, 6);
+				const sorted_random_number = random_number.sort((a, b) => a - b);
+				const lotto = new Lotto(sorted_random_number);
+				lottos.push(lotto.numbers);
+			});
+
+		return lottos;
 	}
 }
 
