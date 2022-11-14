56223f00dcce234a1172aa654f2d58b37b88d1d8
rachel490 <ccori12@naver.com>
2022-11-15 00:23:33 +0900

    feat: lotto numbers getter 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,22 @@
 class Lotto {
-  #numbers;
+	#numbers;
 
-  constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
-  }
+	constructor(numbers) {
+		this.validate(numbers);
+		this.#numbers = numbers;
+	}
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
+	validate(numbers) {
+		if (numbers.length !== 6) {
+			throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+		}
+	}
 
-  // TODO: 추가 기능 구현
+	get numbers() {
+		return this.#numbers;
+	}
+
+	// TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
