3d5c66500857920c04240a4acf1f76eb81b8a2a9
Minhye Kang <alsgp9898@naver.com>
2022-12-08 22:54:31 +0900

    style: 오타 수정 및 prettier 적용

--- src/Domain/Bonus.js
+++ src/Domain/Bonus.js
@@ -1,6 +1,6 @@
 const { throwError, isOutOfRange, findIntersection } = require("../util");
 const ERROR = Object.freeze({
-  not_number: "보너스 번호는 한 자라 숫자여야 합니다.",
+  not_number: "보너스 번호는 한 자리 숫자여야 합니다.",
   duplicate: "보너스 번호가 로또 번호와 중복됩니다.",
   out_of_range: "보너스 번호는 1~45까지의 숫자여야 합니다.",
 });
@@ -24,7 +24,11 @@ class Bonus {
   findFiveMatchLotto(totalUserLotto) {
     this.fiveMatchLotto = [];
     totalUserLotto.map((eachUserLotto) => {
-      if (findIntersection(new Set(eachUserLotto), new Set(this.#winningLotto)) === 5) {
+      if (findIntersection(
+          new Set(eachUserLotto),
+          new Set(this.#winningLotto)
+          ) === 5
+      ) {
         this.fiveMatchLotto.push(eachUserLotto);
       }
     });
