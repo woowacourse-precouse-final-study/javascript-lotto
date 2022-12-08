6631647e12cb8e30f381732b2095d2ea6ba905cb
murpin <qw04011@gmail.com>
2022-12-08 19:43:22 +0900

    feat: 로또 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,61 @@
+const { 
+  isDuplicateNumber,
+  isNumberLengthCheck,
+} = require('./Controllers/Validate');
+const { 
+  compareLottoNumber,
+  calcLottoResultMoney,
+  calcLottoResultCount,
+} = require('./Controllers/CalcLotto');
+const { printResultHeader } = require('./Views/OutputView');
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = String(numbers);
+    this.lottoResult = [0,0,0,0,0];
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  getLottoNumber() {
+    return this.lottoResult;
+  }
+
+  lottoResultCalc() {
+    printResultHeader();
+    calcLottoResultCount(this.lottoResult);
+    return calcLottoResultMoney(this.lottoResult);
+  }
+
+  checkJackpotLotto(lottoInfo, BonusNumber) {
+    lottoInfo.forEach((lotto) => {
+      let correctCount = compareLottoNumber(String(lotto).split(','), this.#numbers.split(','));
+      if (correctCount === '0') return;
+      this.calcLottoResultCount(correctCount, lotto, this.lottoResult, BonusNumber);
+    });
+  }
+
+  calcLottoResultCount(correctCount, lotto, lottoResult, BonusNumber) {
+    switch(true){
+      case correctCount === 5:
+        if (lotto.includes(Number(BonusNumber))) {
+          lottoResult[3]++;
+          break;
+        }
+        lottoResult[2]++;
+        break;
+      case correctCount === 6:
+        lottoResult[4]++;
+        break;
+      default:
+        lottoResult[correctCount - 3]++;
+        break;
     }
   }
-
-  // TODO: 추가 기능 구현
+  validate(numbers) {
+    isDuplicateNumber(numbers);
+    isNumberLengthCheck(numbers);    
+  }
 }
 
 module.exports = Lotto;
