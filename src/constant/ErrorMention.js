fd09ff4d5ba65400d2ac83b22ba09719c5ceccf7
murpin <qw04011@gmail.com>
2022-12-08 19:42:06 +0900

    refactor: 상수 분리

---
+++ src/constant/ErrorMention.js
@@ -0,0 +1,9 @@
+const LOTTO_ERROR_MENTION = {
+  lotto_number_length: "[ERROR] 로또 번호는 6개여야 합니다.",
+  duplicate_lotto_number: "[ERROR] 중복된 수를 입력하면 안됩니다.",
+  money_size_thousand: "[ERROR] 1000원 단위의 수를 입력해야합니다!",
+  bonus_number_range: "[ERROR] 0~45 범위의 숫자를 입력해주세요",
+  bonus_number_length: "[ERROR] 1개의 숫자만 입력해주세요",
+}
+
+module.exports = LOTTO_ERROR_MENTION;
