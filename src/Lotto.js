4dd5aa9d2c4020b1684c2beee1b1d12cc99e74fb
ming0830 <minsun0830@naver.com>
2022-12-10 20:31:59 +0900

    feat : lotto클래스에서 구매 수량을 반환하는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,31 +1,22 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-
 class Lotto {
-  #numbers;
+  #numbers; // 당첨번호
 
   constructor(numbers) {
-    this.validate(numbers);
+    // this.validate(numbers);
     this.#numbers = numbers;
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }  
-    const numPattern =/^[0-9]*$/;	
-    if (numPattern.test(numbers)) {
-      throw new Error("[ERROR] 숫자만 입력하세요.");
-    }
-    const uniqueNums = new Set(numbers);
-    if (uniqueNums.size !== numbers.length) {
-      throw new Error("[ERROR] 중복 되지 않는 숫자를 입력하세요.");
-    }
-    numbers.forEach((number) => {
-      if (number < 1 || number > 45) {
-        throw new Error("[ERROR] 1~45 사이에 숫자만 입력하세요.");
-      }
-  });
+  purchaseQuantity(amount) {
+    return amount/1000
   }
+
+  // validate(numbers) {
+  //   if (numbers.length !== 6) {
+  //     throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  //   }
+  // }
+
+  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
